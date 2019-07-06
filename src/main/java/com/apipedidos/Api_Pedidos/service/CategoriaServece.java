47360884b8edb5f50d26ded2469d2d79c7fc84bc
Lucas Costa <lucasgti91@gmail.com>
2019-07-05 23:23:46 -0300

    Ajustes no endpoint \ categorias\{id}

--- src/main/java/com/apipedidos/Api_Pedidos/service/CategoriaServece.java
+++ src/main/java/com/apipedidos/Api_Pedidos/service/CategoriaServece.java
@@ -7,15 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.apipedidos.Api_Pedidos.domain.Categoria;
 import com.apipedidos.Api_Pedidos.repository.CategoriaRepository;
+import com.apipedidos.Api_Pedidos.service.exception.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaServece {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
+																  ", Tipo: " + Categoria.class.getName()));
+							
 		}
 }
